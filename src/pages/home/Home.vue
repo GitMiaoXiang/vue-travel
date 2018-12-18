a5b5789929a05fc9329789b87760e2d418c260bd
GitMiaoXiang <14796635306@163.com>
2018-12-18 11:08:16 +0800

    轮播首页完成

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-swiper></home-swiper>
-    <div>test</div>
+    <home-swiper :list="list"></home-swiper>
   </div>
 </template>
 
@@ -11,7 +10,21 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
-  components: {HomeSwiper, HomeHeader}
+  components: {HomeSwiper, HomeHeader},
+  data:function () {
+    return {
+      list:[
+        {
+          id:1,
+          imgUrl:require('../../assets/images/1.png')
+        },
+        {
+          id:2,
+          imgUrl:require('../../assets/images/2.png')
+        }
+      ]
+    }
+  }
 }
 </script>
 
